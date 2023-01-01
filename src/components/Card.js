af5aa95892756019dcd62f0c7ad06e8e3cacc2e1
surahj <odebodeopeyemi@gmail.com>
2023-01-01 02:08:40 +0100

    Fix error

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ name, email, id }) => {
   // const { name, email, id } = props;
   return (
     <div className='bg-light-green dib pa2 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}50x50`} alt="robots" />
+      <img src={`https://robohash.org/${id}`} style={{ width: "250px", height: "200px" }} alt="robots" />
       <div>
         <h2> {name}</h2>
         <p> {email} </p>
